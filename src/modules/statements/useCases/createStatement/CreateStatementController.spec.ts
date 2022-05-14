8b93f42ef3d454c90a28d3ded9a2a79471462b4a
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-05-14 18:21:21 -0300

    create end-to-end withdraw/deposit test

--- src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -25,39 +25,81 @@ describe("Create UserProfile Controller", () => {
     await connection.close();
   });
 
-  it("should be able deposit when authenticated", async () => {
+  it("should be able to deposit when user is authenticated", async () => {
     const authenticateResponse = await request(app)
       .post("/api/v1/sessions")
       .send({ email: user.email, password: user.password });
 
     const { token } = authenticateResponse.body;
 
+    const amount = 250;
+
     const response = await request(app)
-      .get("/api/v1/statements/balance")
+      .post("/api/v1/statements/deposit")
+      .send({ amount, description: "rent payment" })
       .set({ Authorization: `Bearer ${token}` });
 
-    console.log(response.body);
-
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty("statement");
-    expect(response.body).toHaveProperty("balance");
+    expect(response.status).toBe(201);
+    expect(response.body.type).toEqual("deposit");
+    expect(response.body.amount).toEqual(amount);
   });
 
-  it("should be able get user statement and balance", async () => {
+  it("should not be able to deposit when user is not authenticated", async () => {
+    const token = "invalid_token9023094019230490123490";
+    const amount = 250;
+
+    const response = await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({ amount, description: "rent payment" })
+      .set({ Authorization: `Bearer ${token}` });
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should be able to withdraw when user is authenticated", async () => {
     const authenticateResponse = await request(app)
       .post("/api/v1/sessions")
       .send({ email: user.email, password: user.password });
 
     const { token } = authenticateResponse.body;
 
+    const amount = 250;
+
+    await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({ amount, description: "salary" })
+      .set({ Authorization: `Bearer ${token}` });
+
     const response = await request(app)
-      .get("/api/v1/statements/balance")
+      .post("/api/v1/statements/withdraw")
+      .send({ amount, description: "bills" })
       .set({ Authorization: `Bearer ${token}` });
 
     console.log(response.body);
 
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty("statement");
-    expect(response.body).toHaveProperty("balance");
+    expect(response.status).toBe(201);
+    expect(response.body.type).toEqual("withdraw");
+    expect(response.body.amount).toEqual(amount);
+  });
+
+  it("should not be able to withdraw with an insufficient balance", async () => {
+    const authenticateResponse = await request(app)
+      .post("/api/v1/sessions")
+      .send({ email: user.email, password: user.password });
+
+    const { token, user: authenticatedUser } = authenticateResponse.body;
+
+    await connection.query(
+      `DELETE FROM statements WHERE user_id='${authenticatedUser.id}'`
+    );
+
+    const amount = 500;
+
+    const response = await request(app)
+      .post("/api/v1/statements/withdraw")
+      .send({ amount, description: "bills" })
+      .set({ Authorization: `Bearer ${token}` });
+
+    expect(response.status).toBe(400);
   });
 });
